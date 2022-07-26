d29e3455fcb4f3a07332cd6bf7d9f29613ba94ee
Olya <Zlobinaolya87@gmail.com>
2022-07-26 07:12:01 +0300

    task done

---
+++ FinalTask/Program.cs
@@ -0,0 +1,26 @@
+﻿void CreatArrayOfStrins(string[] Array)
+{
+    string[] NewArray = new string[Array.Length];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            NewArray[i] = Array[i];
+            Console.Write($"{NewArray[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
+void PrintArrayOfStrings(string[] Arr)
+{
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        Console.Write($"{Arr[i]}, ");
+    }
+    Console.Write($" -> ");
+}
+
+string[] MyArray = {"один", "два", "три", "четыре", "пять", "шесть", "1", "22", "333", "4444"};
+PrintArrayOfStrings(MyArray);
+CreatArrayOfStrins(MyArray);
+
